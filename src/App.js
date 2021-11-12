28635ccb4ca26f2b4b60cbc0bda694b89fc9001a
NorVirae <norbertmbafrank@gmail.com>
2021-11-12 10:16:47 +0100

    just added all the text component of navbar

--- src/App.js
+++ src/App.js
@@ -3,8 +3,36 @@ import './App.css';
 
 function App() {
   return (
+    
     <div className="container">
-      App
+      <nav className="className">
+
+
+
+
+
+
+        <div className={"nav-brand"}>
+          <img src={logo} className={"logo"} alt="the logo" />
+          <p className={"slogan"}>People Ops at Status</p>
+        </div>
+
+        <div className={"navbar"}>
+          <ul className="">
+            <li>DIRECT</li>
+            <li>GET STARTED!</li>
+            <li>PERKS</li>
+            <li>WORKING HERE</li>
+            <li>HEALTH</li>
+            <li>TRAVEL</li>
+            <li>HIRING</li>
+            <li>TEAM LEAD MANUAL</li>
+            <li>FINANCE</li>
+
+          </ul>
+
+        </div>
+      </nav>
     </div>
   
   );
