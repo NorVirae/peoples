c58f667b8cf5196fac0f587248bd68b1e57cd4c9
NorVirae <norbertmbafrank@gmail.com>
2021-11-12 10:08:46 +0100

    setting up the development environ adding the images and vids

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './assets/images/logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="container">
+      App
+    </div>
+  
+  );
+}
+
+export default App;
